4f588a044429fe98f14987ffa085d72e80a0785f
Miguel Bassila <miguelbassila@me.com>
2014-11-13 14:23:40 -0200

    Add Fee test file

---
+++ src/test/java/com/miguelbassila/bank/model/FeeTest.java
@@ -0,0 +1,14 @@
+package com.miguelbassila.bank.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FeeTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
