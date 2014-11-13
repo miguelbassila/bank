872ef3c2be16e3b51cf8dbee07085be6c382f467
Miguel Bassila <miguelbassila@me.com>
2014-11-13 18:56:51 -0200

    Add FeeD which implements Fee interface

---
+++ src/main/java/com/miguelbassila/bank/model/FeeD.java
@@ -0,0 +1,13 @@
+package com.miguelbassila.bank.model;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class FeeD implements Fee {
+
+  public BigDecimal calculate(Transfer transfer) {
+   //TODO implement this
+    return new BigDecimal(0);
+  }
+ }
