ee0009c5d2b5d8eb54b86e3e50ef310f7e89e3cd
Miguel Bassila <miguelbassila@me.com>
2014-11-13 13:53:59 -0200

    Add Transfer test file

---
+++ src/test/java/com/miguelbassila/bank/model/TransferTest.java
@@ -0,0 +1,14 @@
+package com.miguelbassila.bank.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TransferTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
