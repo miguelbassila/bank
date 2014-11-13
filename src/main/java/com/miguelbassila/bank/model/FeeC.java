516aae7e722fd546066cd37e42e99d8e2abd2ac0
Miguel Bassila <miguelbassila@me.com>
2014-11-13 17:34:48 -0200

    Add FeeC which implements Fee interface

---
+++ src/main/java/com/miguelbassila/bank/model/FeeC.java
@@ -0,0 +1,12 @@
+package com.miguelbassila.bank.model;
+
+import java.math.BigDecimal;
+
+public class FeeC implements Fee {
+
+  public BigDecimal calculate(Transfer transfer) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
