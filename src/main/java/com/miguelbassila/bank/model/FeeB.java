05db914287228f051ed01420cda32060fbc4ce9b
Miguel Bassila <miguelbassila@me.com>
2014-11-13 16:49:59 -0200

    Add implementation to calculate method of FeeB

--- src/main/java/com/miguelbassila/bank/model/FeeB.java
+++ src/main/java/com/miguelbassila/bank/model/FeeB.java
@@ -1,12 +1,34 @@
 package com.miguelbassila.bank.model;
 
 import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class FeeB implements Fee {
 
   public BigDecimal calculate(Transfer transfer) {
-    // TODO Auto-generated method stub
-    return null;
+    Calendar dateSchedule = transfer.getDateSchedule();
+    Calendar today = GregorianCalendar.getInstance();
+    
+    long days = calculateDays(today, dateSchedule); 
+    
+    if (days >=30){
+      return BigDecimal.valueOf(10);
+    }
+    
+    return BigDecimal.valueOf(8);
+  }
+
+  private long calculateDays(Calendar today, Calendar dateSchedule) {
+    
+    Instant instantToday = today.toInstant();
+    Instant instantDateSchedule = dateSchedule.toInstant();
+    
+    Duration duration = Duration.between(instantToday, instantDateSchedule);
+    
+    return duration.toDays();
   }
 
 }
