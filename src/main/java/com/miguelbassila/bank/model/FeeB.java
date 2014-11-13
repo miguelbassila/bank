ec2249e4b8a27ced9694e25a2c47f0460fbbceaa
Miguel Bassila <miguelbassila@me.com>
2014-11-13 14:33:04 -0200

    Add FeeB which implements Fee interface

---
+++ src/main/java/com/miguelbassila/bank/model/FeeB.java
@@ -0,0 +1,12 @@
+package com.miguelbassila.bank.model;
+
+import java.math.BigDecimal;
+
+public class FeeB implements Fee {
+
+  public BigDecimal calculate(Transfer transfer) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
