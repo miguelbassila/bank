e0dc82cb11d1f7170bfd88fbe1dfea22819bfc20
Miguel Bassila <miguelbassila@me.com>
2014-11-14 01:08:53 -0200

    Add pseudo code to FeeD

--- src/main/java/com/miguelbassila/bank/model/FeeD.java
+++ src/main/java/com/miguelbassila/bank/model/FeeD.java
@@ -7,7 +7,15 @@ import java.util.GregorianCalendar;
 public class FeeD implements Fee {
 
   public BigDecimal calculate(Transfer transfer) {
-   //TODO implement this
+  
+    if (transfer.getAmount().c <= 25000){
+      calculate with fee A
+    } else if (amount > 25000 && amount <= 120.000){
+      calculate with fee B
+    } else {
+      calculate with fee C
+    }
+    
     return new BigDecimal(0);
   }
  }
