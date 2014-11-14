7d5908247212a800e2bc6834b8a85c19fa4c9645
Miguel Bassila <miguelbassila@me.com>
2014-11-13 23:59:16 -0200

    Fix get name

--- src/main/java/com/miguelbassila/bank/model/FeeA.java
+++ src/main/java/com/miguelbassila/bank/model/FeeA.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class FeeA implements Fee {
 
   public BigDecimal calculate(Transfer transfer) {
-    BigDecimal amount = transfer.getAmmount();
+    BigDecimal amount = transfer.getAmount();
     BigDecimal fee = amount.multiply(BigDecimal.valueOf(0.03)).add(BigDecimal.valueOf(2.00));
     
     return fee;
